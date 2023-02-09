50c8c2de843351b3f510abf940b9693844921295
N <34268416+ziloen@users.noreply.github.com>
2023-02-09 09:41:03 +0800

    chore: update Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,14 @@
-export default function Counter({ initial }: { initial: number }) {
+export default function Counter({ initial }: { initial?: number }) {
   const { count, increment, decrement } = useCounter(initial ?? 0)
 
   return (
     <div>
       {count}
-      <button className="inc" onClick={() => increment()}>
-      +
+      <button className="inc" onClick={increment}>
+        +
       </button>
-      <button className="dec" onClick={() => decrement()}>
-      -
+      <button className="dec" onClick={decrement}>
+        -
       </button>
     </div>
   )
